b017656fd4626ff2b17cb4c1101404ed431b78e2
laumartin <lauratemar@gmail.com>
2020-12-03 12:11:20 +0000

    Connect Flasks to MongoDB. Communicating Flask with Mongo, by installing library 'flask-pymongo' and 'dnsython' package in order to use the Mongo SRV connection string. Updated the requirements.txt file for Heroku. Configuring the app to grab the database name, key and connection string Mongo_URI in the heroku site. Setting up an instance of PyMongo by defining this Flask 'app' object, creating a function with a decorator that includes a route to that function to ensure our Flask app is communicating with the Mongo database. That function will render a template called 'tasks.html', creating the templates directory for that. The template generates data visible to our users from our tasks collection on MongoDB, by using the Jinja templating language to loop through each task item from within our collection. Importing additional functionality from Flask at the top

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo 
+# MongoDB stores its data in a JSON-like format called BSON. To find documents from MongoDB, we need to be able to render the ObjectId,
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+# first configuration will be used to grab the database name
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# we need to configure the actual connection string
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+#grab our SECRET_KEY, which is a requirement when using some of the functions from Flask
+app.secret_key = os.environ.get("SECRET_KEY")
+# This is the Flask 'app' object defined above, to ensure  Flask app is communicating with the Mongo database.
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def hello():
-    return "Hello world.. again"
+# creating a function with a decorator that includes a route to that function. The routing is a string that, 
+# when we attach it to a URL, will redirect to a particular function in our Flask app. 
+# added directly beneath our existing default root, so that either URL will direct the user to the same page.
+@app.route("/get_tasks")
+def get_tasks():
+    # On this tasks template, we want to generate data from our tasks collection on MongoDB, visible to our users.
+    # This will find all documents from the tasks collection, and assign them to our new 'tasks' variable.
+    tasks = mongo.db.tasks.find()
+    #Along with the rendering of the template, we'll pass that tasks variable through to the template. 
+    # The first 'tasks' is what the template will use, and that's equal to the second 'tasks', which is our variable defined above.
+    return render_template("tasks.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
