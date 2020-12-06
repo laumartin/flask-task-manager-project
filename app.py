75f1bc24337c76b7d87e8fef044bb0e5ff4f4f4c
laumartin <lauratemar@gmail.com>
2020-12-06 14:54:20 +0000

    Setting the User Profile Page creating the profile.html Adding session function on app.py to retrieve username once logged. Updating links on base.html to point to profile.html

--- app.py
+++ app.py
@@ -66,6 +66,8 @@ def register():
         # function imported from flask at the top
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -89,6 +91,8 @@ def login():
                 # that we have called user.
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome,{}".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -101,6 +105,17 @@ def login():
 
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # create new username variable that is the user found from db
+    # the session variable in [] must be called 'user' to be consistent.
+    # to avoid retreive the entire record from db,and just get the username
+    # stored, include more []to only grab the 'username' key field from record
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+    # the first username is what the template expect to retrieve in html file
+    # the sedond username is what has been defined in the line above
+    return render_template("profile.html", username=username)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
