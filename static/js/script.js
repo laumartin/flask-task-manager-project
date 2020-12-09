c859bde2ffdc8fb26fc7054d73fc5083c26d409c
laumartin <lauratemar@gmail.com>
2020-12-09 11:59:56 +0000

    Adding task form input fields in add_task.html using Materialize elements and classes and updating the scripts.js file with the correspondent jquery snippets to allow initialize Materialize elements

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        // This are built-in options to customize the datepicker element from Materialize
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
